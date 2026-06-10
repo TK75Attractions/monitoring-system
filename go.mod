d64110dcecf7b0334228277f96beae5e27409aa8
taichi765 <taichi0209.youtub@gmail.com>
2026-06-10 13:49:27 +0000

    feat: Add battery percentage acquision on windows

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.26.2
 
 require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
-	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/sys v0.46.0
 )
--- platform_windows.go
+++ platform_windows.go
@@ -1,5 +1,30 @@
 package main
 
-func getBatteryPercentage() int {
-	panic("todo")
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+type SystemPowerStatus struct {
+	ACLineStatus        uint8
+	BatteryFlag         uint8
+	BatteryLifePercent  uint8
+	SystemStatusFlag    uint8
+	BatteryLifeTime     uint32
+	BatteryFullLifeTime uint32
+}
+
+// Returns percentage of remainig battery.
+func getBatteryPercentage() uint8 {
+	proc := windows.NewLazySystemDLL("kernel32.dll").NewProc("GetSystemPowerStatus")
+	var s SystemPowerStatus
+
+	ret1, _, err := proc.Call(uintptr(unsafe.Pointer(&s)))
+	if ret1 != 0 {
+		fmt.Fprintf(os.Stderr, "error occured while calling GetSystemPowerStatus(): %s\n", err)
+	}
+	return s.BatteryLifePercent
 }
